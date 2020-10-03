59451effb0901caa74e25ae4d6dc6a5de880a59d
João R C Almeida <joaorca@gmail.com>
2020-10-03 00:08:08 -0300

    Implementando funcoes handle dos inputs

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -31,7 +31,48 @@ const globalRadioOr = $('#radioOr');
  */
 function start() {
   console.log("Let's Start");
+
+  /**
+   * Adicionando eventos aos inputs, checkboxes e radio buttons
+   */
+  globalInputName.addEventListener('input', handleInputChange);
+
+  globalCheckboxJava.addEventListener('input', handleCheckboxClick);
+  globalCheckboxJavaScript.addEventListener('input', handleCheckboxClick);
+  globalCheckboxPython.addEventListener('input', handleCheckboxClick);
+
+  globalRadioAnd.addEventListener('input', handleRadioClick);
+  globalRadioOr.addEventListener('input', handleRadioClick);
 }
+
+/**
+ * Em toda mudança no texto de input,
+ * haverá uma nova filtragem e renderização
+ * de dev's
+ */
+function handleInputChange() {}
+
+/**
+ * Refletimos os cliques de cada checkbox no state.
+ * Identificamos qual checkbox foi clicado através
+ * do respectivo id. Em seguida, filtramos os dev's.
+ */
+function handleCheckboxClick({ target }) {
+  const checkboxId = target.id;
+  globalState[checkboxId] = !globalState[checkboxId];
+}
+
+/**
+ * Aqui garantimos que uma e somente uma das opções
+ * de radio de state permaneça como true. Em seguida,
+ * filtramos os dev's
+ */
+function handleRadioClick({ target }) {
+  const radioId = target.id;
+  globalState.radioAnd = radioId === 'radioAnd';
+  globalState.radioOr = radioId === 'radioOr';
+}
+
 /**
  * Inicializando o app
  */
